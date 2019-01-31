943cb85ce16d9efdf93eac671229e01ec671846b
Jin Yoon <jinny.yoon@samsung.com>
2019-02-01 08:18:37 +0900

    Remove unnecessary files for the template

--- testiot1/inc/log.h
+++ testiot1/inc/log.h
@@ -17,7 +17,7 @@ extern "C" {
 #ifdef LOG_TAG
 #undef LOG_TAG
 #endif
-#define LOG_TAG "ST-THINGS-APP"
+#define LOG_TAG "testiot1"
 
 #define ERR(fmt, args...) dlog_print(DLOG_ERROR, LOG_TAG, "%s : %s(%d) > " fmt" \n", rindex(__FILE__, '/') + 1, __func__, __LINE__, ##args)
 #define WARN(fmt, args...) dlog_print(DLOG_WARN, LOG_TAG, "%s : %s(%d) > " fmt" \n", rindex(__FILE__, '/') + 1, __func__, __LINE__, ##args)
