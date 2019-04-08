1526719a384d1dcb1b9c096211b45ad682decc68
Eunyoung Lee <silverlee2@gmail.com>
2019-04-08 16:29:12 +0900

    Update app : testiot1 -> testiot2

--- testiot2/inc/log.h
+++ testiot2/inc/log.h
@@ -28,7 +28,7 @@ extern "C" {
 #ifdef LOG_TAG
 #undef LOG_TAG
 #endif
-#define LOG_TAG "testiot1"
+#define LOG_TAG "testiot2"
 
 #define ERR(fmt, args...) dlog_print(DLOG_ERROR, LOG_TAG, "%s : %s(%d) > " fmt" \n", rindex(__FILE__, '/') + 1, __func__, __LINE__, ##args)
 #define WARN(fmt, args...) dlog_print(DLOG_WARN, LOG_TAG, "%s : %s(%d) > " fmt" \n", rindex(__FILE__, '/') + 1, __func__, __LINE__, ##args)
@@ -40,14 +40,14 @@ extern "C" {
 
 #define retv_if(expr, val) do { \
     if(expr) { \
-    	dlog_print(DLOG_INFO, "NUBISON", "(%s) -> %s() return", #expr, __FUNCTION__); \
+		dlog_print(DLOG_ERROR, LOG_TAG, "%s : %s(%d) > (%s) -> %s() return", rindex(__FILE__, '/') + 1, __func__, __LINE__, #expr, __FUNCTION__, __LINE__); \
         return (val); \
     } \
 } while (0)
 
 #define ret_if(expr) do { \
     if(expr) { \
-    	dlog_print(DLOG_INFO, "NUBISON", "(%s) -> %s() return", #expr, __FUNCTION__); \
+		dlog_print(DLOG_ERROR, LOG_TAG, "%s : %s(%d) > (%s) -> %s() return ", rindex(__FILE__, '/') + 1, __func__, __LINE__, #expr, __FUNCTION__, __LINE__); \
         return; \
     } \
 } while (0)
